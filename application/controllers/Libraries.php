44b6e2975273308e2a9f7511ea62126f02d427b6
Aziz Rahaman <aziz.cse.cu@gmail.com>
2017-12-26 15:55:12 +0600

    Second catagory code start

--- application/controllers/Libraries.php
+++ application/controllers/Libraries.php
@@ -16,83 +16,157 @@ class Libraries extends CI_Controller {
 
 // Organization Librasry Starts
 
-	public function Organization()
-	{
-		$sql = $this->db->get('tbl_organization')->result();
-		$msgerr = $this->session->flashdata('msgerr');
-		$msgok = $this->session->flashdata('msgok');
-		$msgdel = $this->session->flashdata('msgdel');
-		$msgupdate = $this->session->flashdata('msgupdate');
-		$data = [
-			'orgs' => $sql,
-			'msgerr' => $msgerr,
-			'msgok' => $msgok,
-			'msgdel' => $msgdel,
-			'msgupdate' => $msgupdate
-		];
-		$this->load->view('header');
-		$this->load->view('sidebar');
-		$this->load->view('libraries/liborg', $data);
-		$this->load->view('footer');
-	}
-
-	public function AddOrg()
-	{
-		$this->form_validation->set_rules('orgname', 'OrgName', 'trim|required');
-		$this->form_validation->set_rules('orgaddr', 'Address', 'trim|required');
-		$this->form_validation->set_rules('orgdetails', 'Details', 'trim');
-
-		if ($this->form_validation->run() == FALSE) {
-			$this->session->set_flashdata('msgerr', 'Something went wrong! Organization not inserted!');
-		} else {
-			$orgname = $this->input->post('orgname');
-			$orgaddr = $this->input->post('orgaddr');
-			$orgdetails = $this->input->post('orgdetails');
-
-			$add = array('fld_orgname' => $orgname, 'fld_address' => $orgaddr, 'fld_details' => $orgdetails);
-			$this->db->insert('tbl_organization', $add);
-			$this->session->set_flashdata('msgok', 'Organization addred successfully!');
+		public function Organization()
+		{
+			$sql = $this->db->get('tbl_organization')->result();
+			$msgerr = $this->session->flashdata('msgerr');
+			$msgok = $this->session->flashdata('msgok');
+			$msgdel = $this->session->flashdata('msgdel');
+			$msgupdate = $this->session->flashdata('msgupdate');
+			$data = [
+				'orgs' => $sql,
+				'msgerr' => $msgerr,
+				'msgok' => $msgok,
+				'msgdel' => $msgdel,
+				'msgupdate' => $msgupdate
+			];
+			$this->load->view('header');
+			$this->load->view('sidebar');
+			$this->load->view('libraries/liborg', $data);
+			$this->load->view('footer');
 		}
-		redirect('Libraries/Organization');
-	}
 
-	public function DelOrg()
-	{
-		$orgid = $this->input->get('uid');
-		$this->Azmodal->delete('tbl_organization','fld_uid',$orgid);
-		if ($this->db->affected_rows()>0) {
-			$this->session->set_flashdata('msgdel', 'Organization Deleted Successfully!');
-		} else {
-			$this->session->set_flashdata('msgdel', 'Somethig wrong happen in database! Contact developer.');
+		public function AddOrg()
+		{
+			$this->form_validation->set_rules('orgname', 'OrgName', 'trim|required');
+			$this->form_validation->set_rules('orgaddr', 'Address', 'trim|required');
+			$this->form_validation->set_rules('orgdetails', 'Details', 'trim');
+
+			if ($this->form_validation->run() == FALSE) {
+				$this->session->set_flashdata('msgerr', 'Something went wrong! Organization not inserted!');
+			} else {
+				$orgname = $this->input->post('orgname');
+				$orgaddr = $this->input->post('orgaddr');
+				$orgdetails = $this->input->post('orgdetails');
+
+				$add = array('fld_orgname' => $orgname, 'fld_address' => $orgaddr, 'fld_details' => $orgdetails);
+				$this->db->insert('tbl_organization', $add);
+				$this->session->set_flashdata('msgok', 'Organization addred successfully!');
+			}
+			redirect('Libraries/Organization');
 		}
-		redirect('Libraries/Organization');
-	}
 
-	public function UpdateOrg()
-	{
-		$this->form_validation->set_rules('orgid', 'OrgID', 'trim|required');
-		$this->form_validation->set_rules('orgname', 'OrgName', 'trim|required');
-		$this->form_validation->set_rules('orgaddr', 'Address', 'trim|required');
-		$this->form_validation->set_rules('orgdetails', 'Details', 'trim');
-
-		if ($this->form_validation->run() == FALSE) {
-			$this->session->set_flashdata('msgupdate', 'Something went wrong! Organization not updated!');
-		} else {
-			$orgid = $this->input->post('orgid');
-			$orgname = $this->input->post('orgname');
-			$orgaddr = $this->input->post('orgaddr');
-			$orgdetails = $this->input->post('orgdetails');
-
-			$update = array('fld_orgname' => $orgname, 'fld_address' => $orgaddr, 'fld_details' => $orgdetails);
-			$this->Azmodal->update('tbl_organization', $update, 'fld_uid', $orgid);
-			$this->session->set_flashdata('msgupdate', 'Organization update successfully!');
+		public function DelOrg()
+		{
+			$orgid = $this->input->get('uid');
+			$this->Azmodal->delete('tbl_organization','fld_uid',$orgid);
+			if ($this->db->affected_rows()>0) {
+				$this->session->set_flashdata('msgdel', 'Organization Deleted Successfully!');
+			} else {
+				$this->session->set_flashdata('msgdel', 'Somethig wrong happen in database! Contact developer.');
+			}
+			redirect('Libraries/Organization');
 		}
-		redirect('Libraries/Organization');
-	}
+
+		public function UpdateOrg()
+		{
+			$this->form_validation->set_rules('orgid', 'OrgID', 'trim|required');
+			$this->form_validation->set_rules('orgname', 'OrgName', 'trim|required');
+			$this->form_validation->set_rules('orgaddr', 'Address', 'trim|required');
+			$this->form_validation->set_rules('orgdetails', 'Details', 'trim');
+
+			if ($this->form_validation->run() == FALSE) {
+				$this->session->set_flashdata('msgupdate', 'Something went wrong! Organization not updated!');
+			} else {
+				$orgid = $this->input->post('orgid');
+				$orgname = $this->input->post('orgname');
+				$orgaddr = $this->input->post('orgaddr');
+				$orgdetails = $this->input->post('orgdetails');
+
+				$update = array('fld_orgname' => $orgname, 'fld_address' => $orgaddr, 'fld_details' => $orgdetails);
+				$this->Azmodal->update('tbl_organization', $update, 'fld_uid', $orgid);
+				$this->session->set_flashdata('msgupdate', 'Organization update successfully!');
+			}
+			redirect('Libraries/Organization');
+		}
 
 	// Organization Librasry Ends
 
 	// Division Librasry Starts
+
+		public function Division()
+		{
+			$sql = $this->db->get('tbl_devision')->result();
+			$msgerr = $this->session->flashdata('msgerr');
+			$msgok = $this->session->flashdata('msgok');
+			$msgdel = $this->session->flashdata('msgdel');
+			$msgupdate = $this->session->flashdata('msgupdate');
+			$data = [
+				'divis' => $sql,
+				'msgerr' => $msgerr,
+				'msgok' => $msgok,
+				'msgdel' => $msgdel,
+				'msgupdate' => $msgupdate
+			];
+			$this->load->view('header');
+			$this->load->view('sidebar');
+			$this->load->view('libraries/libdiv', $data);
+			$this->load->view('footer');
+		}
+
+		public function AddDiv()
+		{
+			$this->form_validation->set_rules('orgname', 'OrgName', 'trim|required');
+			$this->form_validation->set_rules('orgaddr', 'Address', 'trim|required');
+			$this->form_validation->set_rules('orgdetails', 'Details', 'trim');
+
+			if ($this->form_validation->run() == FALSE) {
+				$this->session->set_flashdata('msgerr', 'Something went wrong! Organization not inserted!');
+			} else {
+				$orgname = $this->input->post('orgname');
+				$orgaddr = $this->input->post('orgaddr');
+				$orgdetails = $this->input->post('orgdetails');
+
+				$add = array('fld_orgname' => $orgname, 'fld_address' => $orgaddr, 'fld_details' => $orgdetails);
+				$this->db->insert('tbl_organization', $add);
+				$this->session->set_flashdata('msgok', 'Organization addred successfully!');
+			}
+			redirect('Libraries/Organization');
+		}
+
+		public function DelDiv()
+		{
+			$orgid = $this->input->get('uid');
+			$this->Azmodal->delete('tbl_organization','fld_uid',$orgid);
+			if ($this->db->affected_rows()>0) {
+				$this->session->set_flashdata('msgdel', 'Organization Deleted Successfully!');
+			} else {
+				$this->session->set_flashdata('msgdel', 'Somethig wrong happen in database! Contact developer.');
+			}
+			redirect('Libraries/Organization');
+		}
+
+		public function UpdateDiv()
+		{
+			$this->form_validation->set_rules('orgid', 'OrgID', 'trim|required');
+			$this->form_validation->set_rules('orgname', 'OrgName', 'trim|required');
+			$this->form_validation->set_rules('orgaddr', 'Address', 'trim|required');
+			$this->form_validation->set_rules('orgdetails', 'Details', 'trim');
+
+			if ($this->form_validation->run() == FALSE) {
+				$this->session->set_flashdata('msgupdate', 'Something went wrong! Organization not updated!');
+			} else {
+				$orgid = $this->input->post('orgid');
+				$orgname = $this->input->post('orgname');
+				$orgaddr = $this->input->post('orgaddr');
+				$orgdetails = $this->input->post('orgdetails');
+
+				$update = array('fld_orgname' => $orgname, 'fld_address' => $orgaddr, 'fld_details' => $orgdetails);
+				$this->Azmodal->update('tbl_organization', $update, 'fld_uid', $orgid);
+				$this->session->set_flashdata('msgupdate', 'Organization update successfully!');
+			}
+			redirect('Libraries/Organization');
+		}
 
 	// Division Librasry Ends
 
